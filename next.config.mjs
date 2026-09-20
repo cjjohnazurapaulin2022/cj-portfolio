0ba0a8558ba0d9d1221cb52de4aebd4b0eda6f5f
CJ <cjjohnazurapaulin2022@gmail.com>
2026-09-20 23:44:44 +0800

    Add CDN Simple Icons to allowed image domains

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,11 @@ const nextConfig = {
         hostname: "www.google.com",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+        pathname: "**",
+      },
     ],
   },
   sassOptions: {
